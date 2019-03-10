e8c738ee128bb37d4be1c6ea9afae230b71c7a40
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-03-10 17:06:08 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
-python3 bazel_version.py
+
+lftp -c "open -u  $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mkdir /ppc64el/bazel/ubuntu_16.04/latest" 
+lftp -c "open -u  $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mkdir /ppc64el/bazel/ubuntu_14.04/latest"
+python3 bazel-version.py
 if [ $(cat current_version.txt) != $(cat ftp_version.txt) ]
 then
     version=$(cat current_version.txt)
-    mkdir work
-    cd work
-    wget https://github.com/bazelbuild/bazel/releases/download/$version/bazel-$version-dist.zip
-    unzip bazel-$version-dist.zip
-    EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
-    mv output/bazel output/bazel_bin_ppc64le_$version
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/bazel/$OS /home/travis/build/Unicamp-OpenPower/bazel-releases/work/output/bazel_bin_ppc64le_$version" 
     del_version=$(cat delete_version.txt)
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/bazel/$OS/bazel_bin_ppc64le_$del_version" 
+    old_version=$(cat ftp_version.txt)
+    sudo mkdir build
+    cd build
+    sudo wget https://github.com/bazelbuild/bazel/releases/download/$version/bazel-$version-dist.zip
+    sudo unzip bazel-$version-dist.zip
+    sudo EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
+    sudo mv output/bazel output/bazel_bin_ppc64le_$version
+    if [ $version > old_version ]
+    then
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/bazel/$OS/latest /home/travis/build/Unicamp-OpenPower/bazel-releases/build/output/bazel_bin_ppc64le_$version" 
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/bazel/$OS/latest/bazel_bin_ppc64le_$old_version" 
+    fi
+    lftp -c "open -u  $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/bazel/$OS /home/travis/build/Unicamp-OpenPower/bazel-releases/build/output/bazel_bin_ppc64le_$version" 
+    lftp -c "open -u  $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/bazel/$OS/bazel_bin_ppc64le_$del_version" 
 fi
