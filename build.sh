0fd44b58c5ca3ab3e81b727e39fe45a6dd81112c
Luciano Zago <lcnzg7@gmail.com>
2019-10-20 10:29:28 -0300

    Fix directories

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ ftp_version=2
 if [ $github_version != $ftp_version ]
 then
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
-    unzip -q bazel-$github_version-dist.zip
+    unzip -q bazel-$github_version-dist.zip -d bazel
+    cd bazel
     sudo EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
     sudo mv output/bazel output/bazel_bin_ppc64le_$github_version
 #    if [[ $github_version > $ftp_version ]]
