83d66fd9535be71b50dd4176bd725c7fb67bdd15
Luciano Zago <lcnzg7@gmail.com>
2019-10-20 12:49:04 -0300

    Compile new Bazel versions

--- build.sh
+++ build.sh
@@ -7,16 +7,14 @@ ftp_version=2
 
 if [ $github_version != $ftp_version ]
 then
+    wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_16.04/bazel_bin_ppc64le_0.27.2
+    sudo mv bazel_bin_ppc64le_0.27.2 /usr/local/bin/bazel
+    sudo chmod +x /usr/local/bin/bazel
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
     unzip -q bazel-$github_version-dist.zip -d bazel
+    git clone https://github.com/Unicamp-OpenPower/patch-apply
+    ./patch-apply/apply_patch.py bazel patchs
     cd bazel
     sudo EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
     sudo mv output/bazel output/bazel_bin_ppc64le_$github_version
-#    if [[ $github_version > $ftp_version ]]
-#    then
-#        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/bazel/ubuntu_16.04/latest /home/travis/build/Unicamp-OpenPower/bazel-releases/work/output/bazel_bin_ppc64le_$github_version" 
-#        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/bazel/ubuntu_16.04/latest/bazel_bin_ppc64le_$ftp_version" 
-#    fi
-#    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/bazel/ubuntu_16.04 /home/travis/build/Unicamp-OpenPower/bazel-releases/work/output/bazel_bin_ppc64le_$github_version" 
-#    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/bazel/ubuntu_16.04/bazel_bin_ppc64le_$del_version" 
 fi
