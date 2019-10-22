47138ce854b74010b757b688c8ffe9c6f6480ad0
Luciano Zago <lcnzg7@gmail.com>
2019-10-22 15:54:59 -0300

    Version 1.1.0

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ ftp_version=2
 
 if [ $github_version != $ftp_version ]
 then
-    wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_16.04/bazel_bin_ppc64le_0.27.2
+    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_16.04/bazel_bin_ppc64le_0.27.2
     sudo mv bazel_bin_ppc64le_0.27.2 /usr/local/bin/bazel
     sudo chmod +x /usr/local/bin/bazel
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
     unzip -q bazel-$github_version-dist.zip -d bazel
-    git clone https://github.com/Unicamp-OpenPower/patch-apply
-    ./patch-apply/apply_patch.py bazel patchs
+#    git clone https://github.com/Unicamp-OpenPower/patch-apply
+#    ./patch-apply/apply_patch.py bazel patchs
     cd bazel
     sudo EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
     sudo mv output/bazel output/bazel_bin_ppc64le_$github_version
