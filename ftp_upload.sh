acc5d282c7ccf28bad0aa0ff4b9f382964ac8210
Luciano Zago <lcnzg7@gmail.com>
2019-11-26 23:23:51 -0300

    Fix ftp upload

--- ftp_upload.sh
+++ ftp_upload.sh
@@ -2,17 +2,23 @@
 
 set -e
 
-FTP_HOST='oplab9.parqtec.unicamp.br'
-LOCALPATH=$TRAVIS_BUILD_DIR/bazel/output
-REMOTEPATH='/ppc64el/bazel/ubuntu_16.04'
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
 
-# Upload files from LOCALPATH recursively to REMOTEPATH
-lftp -f "
-set dns:order "inet"
-set xfer:use-temp-file yes
-set xfer:temp-file-name *.tmp
-open ftp://$FTP_HOST
-user $USER $PASS
-mirror -R --continue --reverse --no-empty-dirs --no-perms -I bazel_bin_ppc64le* $LOCALPATH $REMOTEPATH
-bye
-"
+if [ $github_version != $ftp_version ]
+then
+    FTP_HOST='oplab9.parqtec.unicamp.br'
+    LOCALPATH=$TRAVIS_BUILD_DIR/bazel/output
+    REMOTEPATH='/ppc64el/bazel/ubuntu_16.04'
+
+    # Upload files from LOCALPATH recursively to REMOTEPATH
+    lftp -f "
+    set dns:order "inet"
+    set xfer:use-temp-file yes
+    set xfer:temp-file-name *.tmp
+    open ftp://$FTP_HOST
+    user $USER $PASS
+    mirror -R --continue --reverse --no-empty-dirs --no-perms -I bazel_bin_ppc64le* $LOCALPATH $REMOTEPATH
+    bye
+    "
+fi
