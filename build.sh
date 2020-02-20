6e689d8df2266c42e83d2cca9c5bb19d57106a38
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-02-20 11:00:53 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 github_version=$(cat github_version.txt)
-ftp_version=$(cat ftp_version.txt)
+ftp_version=1.0.0
 #del_version=$(cat delete_version.txt)
 
 #github_version=$ver
@@ -9,8 +9,8 @@ ftp_version=$(cat ftp_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
-    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_1.0.0
-    sudo mv bazel_bin_ppc64le_1.0.0 /usr/local/bin/bazel
+    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_2.0.0
+    sudo mv bazel_bin_ppc64le_2.0.0 /usr/local/bin/bazel
     sudo chmod +x /usr/local/bin/bazel
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
     unzip -q bazel-$github_version-dist.zip -d bazel
