baf486f7df6fc46a944d7adb96e7c35d58b4b5d0
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-06-12 04:51:53 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-github_version=$(cat github_version.txt)
+#github_version=$(cat github_version.txt)
+github_version=3.1.0
 ftp_version=$(cat ftp_version.txt)
 #del_version=$(cat delete_version.txt)
 
@@ -9,13 +10,11 @@ ftp_version=$(cat ftp_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
-    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_2.0.0
-    sudo mv bazel_bin_ppc64le_2.0.0 /usr/local/bin/bazel
+    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_3.0.0
+    sudo mv bazel_bin_ppc64le_3.0.0 /usr/local/bin/bazel
     sudo chmod +x /usr/local/bin/bazel
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
     unzip -q bazel-$github_version-dist.zip -d bazel
-    rm bazel/src/main/java/com/google/devtools/build/lib/syntax/BUILD
-    mv BUILD bazel/src/main/java/com/google/devtools/build/lib/syntax/
     cd bazel
     sudo EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk ./compile.sh
     sudo mv output/bazel output/bazel_bin_ppc64le_$github_version
