cd37eda90d3cf2a4ce6504cf378fb740866ecf0c
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-07-07 16:38:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #github_version=$(cat github_version.txt)
-github_version=3.2.0
+github_version=3.3.0
 ftp_version=$(cat ftp_version.txt)
 #del_version=$(cat delete_version.txt)
 
@@ -10,16 +10,16 @@ ftp_version=$(cat ftp_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
-    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_3.1.0
-    sudo mv bazel_bin_ppc64le_3.1.0 /usr/local/bin/bazel
+    wget -q https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_${dist_version}/bazel_bin_ppc64le_3.2.0
+    sudo mv bazel_bin_ppc64le_3.2.0 /usr/local/bin/bazel
     sudo chmod +x /usr/local/bin/bazel
     wget -q https://github.com/bazelbuild/bazel/releases/download/$github_version/bazel-$github_version-dist.zip
     unzip -q bazel-$github_version-dist.zip -d bazel
     cd bazel
     ini=$(pwd)
-    rm $ini/src/conditions/BUILD
-    cd $ini/src/conditions/
-    wget https://raw.githubusercontent.com/bazelbuild/bazel/78a29b0d0dfe350ab9513b6eefa9a2ed46c6c865/src/conditions/BUILD
+    #rm $ini/src/conditions/BUILD
+    #cd $ini/src/conditions/
+    #wget https://raw.githubusercontent.com/bazelbuild/bazel/78a29b0d0dfe350ab9513b6eefa9a2ed46c6c865/src/conditions/BUILD
     #rm $ini/src/main/java/com/google/devtools/build/lib/syntax/BUILD
     #cd $ini/src/main/java/com/google/devtools/build/lib/syntax/
     #wget https://raw.githubusercontent.com/bazelbuild/bazel/78a29b0d0dfe350ab9513b6eefa9a2ed46c6c865/src/main/java/com/google/devtools/build/lib/syntax/BUILD
