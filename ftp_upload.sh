4715a10c6b6477226848cd35f41f12ac1f1ebff6
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-08-28 13:40:35 -0300

    Add release from repository

--- ftp_upload.sh
+++ ftp_upload.sh
@@ -10,6 +10,9 @@ then
     FTP_HOST='oplab9.parqtec.unicamp.br'
     LOCALPATH=$TRAVIS_BUILD_DIR/bazel/output
     REMOTEPATH="/ppc64el/bazel/ubuntu_$dist_version"
+    ROOTPATH="/root/rpmbuild/RPMS/ppc64le"
+    REPO1="/repository/debian/ppc64el/bazel"
+    REPO2="/repository/rpm/ppc64le/bazel"
 
     # Upload files from LOCALPATH recursively to REMOTEPATH
     lftp -f "
@@ -21,4 +24,19 @@ then
     mirror -R --continue --reverse --no-empty-dirs --no-perms -I bazel_bin_ppc64le* $LOCALPATH $REMOTEPATH
     bye
     "
+
+    #Upload files from repository
+    if [ ${dist_version} == "18.04" ]
+    then
+      sudo lftp -f "
+      set dns:order "inet"
+      set xfer:use-temp-file yes
+      set xfer:temp-file-name *.tmp
+      open ftp://$FTP_HOST
+      user $USER $PASS
+      mirror -R --continue --reverse --no-empty-dirs --no-perms -I bazel-$github_version-ppc64le.deb $LOCALPATH $REPO1
+      mirror -R --continue --reverse --no-empty-dirs --no-perms -I bazel-* $ROOTPATH $REPO2
+      bye
+      "
+    fi
 fi
---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
+LOCALPATH=$TRAVIS_BUILD_DIR/bazel/output
+
+if [ $github_version != $ftp_version ] && [ ${dist_version} == "18.04" ]
+then
+  cd $TRAVIS_BUILD_DIR
+  git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
+  cd repository-scrips/
+  chmod +x empacotar-deb.sh
+  chmod +x empacotar-rpm.sh
+  mv empacotar-deb.sh $LOCALPATH
+  mv empacotar-rpm.sh $LOCALPATH
+  cd $LOCALPATH
+  ./empacotar-deb.sh bazel bazel_bin_ppc64le_$github_version $github_version "gcc, default-jdk"
+  sudo ./empacotar-rpm.sh bazel bazel_bin_ppc64le_$github_version $github_version "gcc, java-11-openjdk-devel" "Build and test software of any size, quickly and reliably"
+fi
